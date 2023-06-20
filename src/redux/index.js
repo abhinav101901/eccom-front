08c58aea698a60e3e2b8c4eb16dec32b3b9ebd11
abhinav101901 <abhinav8299233838@gmail.com>
2023-06-20 11:44:59 +0530

    day-3

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,1 +1,11 @@
-//hello
+import { configureStore } from "@reduxjs/toolkit";
+import userSliceReducer from "./userSlice";
+import productSlideReducer from "./productSlide";
+
+export const store = configureStore({
+  reducer: {
+    user : userSliceReducer,
+    product : productSlideReducer
+    
+  },
+});
